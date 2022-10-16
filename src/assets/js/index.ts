b78fc56002415841da4c77c064e7ae657de14db2
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2022-10-16 18:15:35 +0400

    assets

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -1,10 +1,17 @@
 import focusBlur from "./focusBlur";
 import swither from "./swither";
 import search from "./search";
+import mobileMenu from "./mobile-menu";
 import "./polifils";
+const windowWidth = document.documentElement.clientWidth;
+if (windowWidth < 390) {
+  const viewport = document.querySelector("meta[name=viewport]");
+  viewport!.setAttribute('content', 'width=390, initial-scale=1');
 
+}
 document.addEventListener("DOMContentLoaded", () => {
   focusBlur();
   swither();
   search();
+  mobileMenu();
 });
