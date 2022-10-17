cdb4f8a05d0884c243c9eb68e0eba1e5fbd81330
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2022-10-17 23:37:55 +0400

    video
    change event for play start

--- src/assets/js/video.ts
+++ src/assets/js/video.ts
@@ -4,7 +4,7 @@ import Swiper from "./swiper/swiper-bundle.min.js";
 export default function startVideo() {
   const videoEl = document.querySelector('video') as HTMLVideoElement;
   if (videoEl) {
-    videoEl.addEventListener('loadeddata', (e) => {
+    videoEl.addEventListener('canplaythrough', (e) => {
       console.log('video loaded');
       videoEl.classList.add('show');
     })
