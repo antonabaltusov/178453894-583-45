f9f0961017f05468a4494e97e8c1c763c3d2f41f
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2022-10-26 13:42:50 +0400

    autoplay

--- src/assets/js/video.ts
+++ src/assets/js/video.ts
@@ -1,11 +1,15 @@
-import Swiper from "./swiper/swiper-bundle.min.js";
-
-
 export default function startVideo() {
   const videoEl = document.querySelector('video') as HTMLVideoElement;
   if (videoEl) {
     videoEl.addEventListener('canplaythrough', (e) => {
       console.log('video loaded');
+      if (videoEl.playing) {
+        console.log('video playing');
+      }
+      else {
+          videoEl.play();
+          console.log('on play');
+      }
       videoEl.play();
       videoEl.classList.add('show');
     })
