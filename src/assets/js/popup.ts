d0458a67128ceb3e19fb5e217840a05dbb9a8b64
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2022-12-08 14:44:10 +0400

    popup

--- src/assets/js/popup.ts
+++ src/assets/js/popup.ts
@@ -1,36 +1,44 @@
-function openPopup(popup: HTMLElement, param:  string) {
-  popup.classList.add('active', param)
+function openPopup(popup: HTMLElement, param: string, overlay: HTMLElement) {
+  popup.classList.add("active", param);
+  overlay.classList.add("active");
 }
-function closePopup(popup: HTMLElement, param:  string[]) {
-  popup.classList.remove('active', ...param)
+function closePopup(popup: HTMLElement, param: string[], overlay: HTMLElement) {
+  popup.classList.remove("active", ...param);
+  overlay.classList.remove("active");
 }
 
 export default function popupFunc() {
-  const popupBlock = document.querySelector('.popup') as HTMLElement;
+  const popupBlock = document.querySelector(".popup") as HTMLElement;
   const arrayClasses: string[] = [];
-  
+  const overlay = document.querySelector(".popup-overlay") as HTMLElement;
   if (popupBlock) {
-    const singInButtons = document.querySelectorAll('[data-popup]') as unknown as HTMLElement[];
-    
-    singInButtons?.forEach(el => {
-      const dataPopup = el.getAttribute('data-popup');
+    const singInButtons = document.querySelectorAll(
+      "[data-popup]"
+    ) as unknown as HTMLElement[];
+
+    singInButtons?.forEach((el) => {
+      const dataPopup = el.getAttribute("data-popup");
       if (dataPopup) {
         if (arrayClasses.indexOf(dataPopup) == -1) {
           arrayClasses.push(dataPopup);
         }
-        el.addEventListener('click', () => {
-          closePopup(popupBlock, arrayClasses);
-          openPopup(popupBlock, dataPopup);
-        })
+        el.addEventListener("click", (e) => {
+          e.stopPropagation();
+          closePopup(popupBlock, arrayClasses, overlay);
+          openPopup(popupBlock, dataPopup, overlay);
+        });
       }
-    })
-
+    });
   }
-  const overlay = document.querySelector('.overlay') as HTMLElement;
-  overlay.addEventListener('click', () => {
-    closePopup(popupBlock, arrayClasses);
-  })
-  document.querySelectorAll('[data-popup-close]').forEach(el => {
-    el.addEventListener('click',  () => closePopup(popupBlock, arrayClasses))
-  })
+
+  overlay.addEventListener("click", (e) => {
+    if (!e.target.closest(".popup")) {
+      closePopup(popupBlock, arrayClasses, overlay);
+    }
+  });
+  document.querySelectorAll("[data-popup-close]").forEach((el) => {
+    el.addEventListener("click", () =>
+      closePopup(popupBlock, arrayClasses, overlay)
+    );
+  });
 }
